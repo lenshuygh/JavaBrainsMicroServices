24b9f278762b764fc0090a9aa74d1e8a1540f5b1
lens <lens.huygh@gmail.com>
2020-07-08 21:35:15 +0200

    init commit 3 services + 1 discovery service client side load balanced

---
+++ movie-catalog-service/movie-catalog-service/src/main/java/be/lens/microservices/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,52 @@
+package be.lens.microservices.javabrains.moviecatalogservice.resources;
+
+import be.lens.microservices.javabrains.moviecatalogservice.model.CatalogItem;
+import be.lens.microservices.javabrains.moviecatalogservice.model.Movie;
+import be.lens.microservices.javabrains.moviecatalogservice.model.UserRating;
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable String userId) {
+
+        //get all rated movie IDs
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+
+        return ratings.getRatings().stream()
+                .map(rating -> {
+                    // for each movie ID call the info service and get the details
+
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+
+                    /*Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();*/
+                    // put them all together
+                    return new CatalogItem(movie.getName(), "A whole lotta dreaming inside dreaming and action too", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+    }
+}
---
+++ ratings-data-service/ratings-data-service/src/test/java/be/lens/microservices/javabrains/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.lens.microservices.javabrains.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
